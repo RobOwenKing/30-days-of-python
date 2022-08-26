7006cdd70038aa5476bc99fea162c087177bd01f
Robert King <robert.o.king@hotmail.co.uk>
2022-08-27 00:57:56 +0100

    feat: Day 02, exercise 1 (variables)

---
+++ 02-variables.py
@@ -0,0 +1,16 @@
+# 30 Days of Python: Day 02
+print("30 Days of Python: Day 02 - Variables")
+
+# 1
+# 1.3 - 1.13
+first_name = "Fakeson"
+last_name = "McFakeface"
+full_name = first_name + " " + last_name
+country = "Countryland"
+city = "Cityburg"
+age = 101
+year = 1879
+is_married = False
+is_true = True
+is_light_on = True
+ma_str, ma_int, ma_set = "multiple assigments", 42, {1, 2}
