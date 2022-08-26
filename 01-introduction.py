7f033e7453685fbbae92e32744d4e6db91478cfb
Robert King <robert.o.king@hotmail.co.uk>
2022-08-26 15:39:14 +0100

    feat: Day 01, exercises 1 and 2

---
+++ 01-introduction.py
@@ -0,0 +1,30 @@
+# 1
+# All in the interactive shell
+
+# 2.1
+# Version check
+
+# 2.2
+# Blame black for the ugly formatting
+# of the ** and line-end comments!
+print(3 + 4)  # Should print 7
+print(3 - 4)  # Should print -1
+print(3 * 4)  # Should print 12
+print(3 / 4)  # Should print 0.75
+print(3**4)  # Should print 81
+print(3 % 4)  # Should print 3
+print(3 // 4)  # Should print 0
+
+# 2.3
+print("Fakename")
+print("McFakeface")
+print("SomeCountry")
+print("I am enjoying 30 days of python")
+
+# 2.4
+# NB: I've removed some repeats
+print(type(10))  # Should print <class 'int'>
+print(type(3.14))  # Should print <class 'float'>
+print(type(4 - 4j))  # Should print <class 'complex'>
+print(type(["Asabeneh", "Python", "Finland"]))  # Should print <class 'list'>
+print(type("Fakename McFakeface"))  # Should print <class 'str'>
