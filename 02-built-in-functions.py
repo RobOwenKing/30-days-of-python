f5ef97dee35e124a033333fd522cefe5dcba828c
Robert King <robert.o.king@hotmail.co.uk>
2022-08-27 17:27:38 +0100

    feat: Day 02, exercise 2.1

---
+++ 02-built-in-functions.py
@@ -0,0 +1,18 @@
+# 30 Days of Python: Day 02
+print("30 Days of Python: Day 02 - Built-In Functions")
+
+# Variables from ./02-variables.py
+first_name = "Fakeson"
+last_name = "McFakeface"
+full_name = first_name + " " + last_name
+year = 1879
+is_true = True
+ma_str, ma_int, ma_set = "multiple assigments", 42, {1, 2}
+
+# 2
+# 2.1
+print("---2.1---")
+print(type(full_name))  # Should print <class 'str'>
+print(type(year))  # Should print <class 'int'>
+print(type(is_true))  # Should print <class 'bool'>
+print(type(ma_set))  # Should print <class 'set'>
