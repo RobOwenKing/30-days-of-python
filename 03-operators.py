f5650bb78c7fb350cdde8a15c1433f63fa6657c8
Robert King <robert.o.king@hotmail.co.uk>
2022-08-28 21:58:41 +0100

    feat: Day 03, exercise 6

--- 03-operators.py
+++ 03-operators.py
@@ -8,11 +8,20 @@ height = 1.84
 # 3
 sqrt_i = 0.7 + 0.7j
 # 4
+"""
 base = input("Enter base: ")
 hght = input("Enter height: ")
 print("The area of the triangle is", 0.5 * float(base) * float(hght))
+"""
 # 5
+"""
 side_a = input("Enter side a: ")
 side_b = input("Enter side b: ")
 side_c = input("Enter side c: ")
 print("The area of the triangle is", float(side_a) + float(side_b) + float(side_c))
+"""
+# 6
+lgth = input("Enter length: ")
+wdth = input("Enter width: ")
+print("The area of the rectangle is", float(lgth) * float(wdth))
+print("The perimetre of the rectangle is", 2 * (float(lgth) + float(wdth)))
