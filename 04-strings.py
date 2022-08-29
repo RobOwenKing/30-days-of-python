e67eb8bf21df46958bc1d6d037a47273955227d9
Robert King <robert.o.king@hotmail.co.uk>
2022-08-29 20:22:29 +0100

    feat: Day 04, exercises 1-2

---
+++ 04-strings.py
@@ -0,0 +1,9 @@
+# 30 Days of Python: Day 04
+print("30 Days of Python: Day 04 - Strings")
+
+# 1
+print("---1---")
+print(" ".join(["Thirty", "Days", "Of", "Python"]))
+# 2
+print("---2---")
+print(" ".join(["Coding", "For", "All"]))
