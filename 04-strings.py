245a4b6d3cdaf3f1ff19926b3ba2962db1f160ab
Robert King <robert.o.king@hotmail.co.uk>
2022-09-15 17:43:59 +0100

    feat: Day 04, exercises 3-9

--- 04-strings.py
+++ 04-strings.py
@@ -7,3 +7,25 @@ print(" ".join(["Thirty", "Days", "Of", "Python"]))
 # 2
 print("---2---")
 print(" ".join(["Coding", "For", "All"]))
+# 3
+company = "Coding For All"
+# 4
+print("---4---")
+print("company =", company)
+# 5
+print("---5---")
+print("length =", len(company))
+# 6
+print("---6---")
+print("all the characters".upper())
+# 7
+print("---7---")
+print("ALL THE CHARACTERS".lower())
+# 8
+print("---8---")
+print("Coding For All".capitalize())
+print("Coding For All".title())
+print("Coding For All".swapcase())
+# 9
+print("---9---")
+print("Coding For All"[:6])
