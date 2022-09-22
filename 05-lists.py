347ec09ec06c0b8d4d8c9a8746dc0a412eb7286f
Robert King <robert.o.king@hotmail.co.uk>
2022-09-22 17:57:33 +0100

    feat: Day 05, exercises 1-4

---
+++ 05-lists.py
@@ -0,0 +1,20 @@
+# 30 Days of Python: Day 04
+print("30 Days of Python: Day 05 - Lists")
+
+# 1
+print("---1---")
+empty_list = []
+print(empty_list)
+# 2
+print("---2---")
+more_than_five = [1, 2, 3, 4, 5, 6, 7]
+print(more_than_five)
+# 3
+print("---3---")
+len_mtf = len(more_than_five)
+print(f"len(more_than_five) = {len_mtf} > 5")
+# 4
+print("---4---")
+print(f"First item: {more_than_five[0]}")
+print(f"Middle item: {more_than_five[len_mtf//2]}")
+print(f"Last item: {more_than_five[-1]}")
